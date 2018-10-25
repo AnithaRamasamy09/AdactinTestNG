9807039c60162f499aa5a5b9975122c534a79953
Anitha <rkanitha@gmail.com>
2018-10-25 08:44:01 +0530

    commiting all

--- src/main/java/base/MainBase.java
+++ src/main/java/base/MainBase.java
@@ -4,14 +4,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MainBase {
 	
 	public static WebDriver driver;
 
 	public static WebDriver getDriver(String url) {
-		System.setProperty("webdriver.gecko.driver", "C:\\Selenium_Anitha\\First Project\\drivers\\geckodriver.exe");
-		driver=new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Lenovo\\git\\AdactinTestNG\\drivers\\geckodriver.exe");
+		driver=new FirefoxDriver();
 		driver.manage().window().maximize();	
 		driver.get(url);
 		return driver;	
